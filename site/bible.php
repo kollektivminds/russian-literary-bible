1dcea7e37a542cb9f8015e2c39568c1af8bd567c
Aaron M. Thompson <amthomps1@gmail.com>
2023-01-26 22:55:57 +0000

    working on list

--- site/bible.php
+++ site/bible.php
@@ -6,13 +6,6 @@ $pageTitle = "RG Bible"
 
 <?php
 include "templates/header.php";
-include "pages/about.html";
-
-
-$xml=simplexml_load_file("../texts/bible/bible.xml") or die("Error: Cannot create object");
-echo $xml->t[0]->b[0]->c[0]->v[0];
-
-#print_r($xml);
-
-include "templates/footer.php";
+include "pages/bible.html";
+include "templates/footer.php"
 ?>
--- site/templates/header.php
+++ site/templates/header.php
@@ -1,23 +1,23 @@
-    <head>
-        <head>
-            <meta charset="UTF-8">
-            <title><?php echo $pageTitle;?></title>
-            <meta name="description" contents="Website for Revolutionary Gospel">
-            <meta name="keywords" content="russian, literature, bible" />
-		    <meta name="robots" content="noindex" />
-            <link rel="stylesheet" href="css/style.css" type="text/css">
-            <link rel="stylesheet" href="css/normalize.css" type="text/css">
-        </head>
-        <body>
-            <header>
-                <nav id="nav-header">
-                    <a href="index.php"><img id="nav-logo" src="./img/favicon-32x32.png"></a>
-                    <ul id="nav-menu">
-                        <li class="nav-item"><a href="index.php">Home</a></li>
-                        <li class="nav-item"><a href="about.php">About</a></li>
-                        <li class="nav-item"><a href="bible.php">Bible</a></li>
-                        <li class="nav-item"><a href="texts.php">Texts</a></li>
-                    </ul>
-                </nav>
-            </header>
-        <div id="page-contents">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $pageTitle;?></title>
+    <meta name="description" contents="Website for Revolutionary Gospel">
+    <meta name="keywords" content="russian, literature, bible" />
+    <meta name="robots" content="noindex" />
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/normalize.css" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+</head>
+<body>
+    <header>
+        <nav id="nav-header">
+            <a href="index.php"><img id="nav-logo" src="./img/favicon-32x32.png"></a>
+            <ul id="nav-menu">
+                <li class="nav-item"><a href="index.php">Home</a></li>
+                <li class="nav-item"><a href="about.php">About</a></li>
+                <li class="nav-item"><a href="bible.php">Bible</a></li>
+                <li class="nav-item"><a href="texts.php">Texts</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div id="page-contents">
